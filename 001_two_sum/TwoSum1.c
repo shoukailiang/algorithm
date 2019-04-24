c84ec22c93a21385130cd8f440796957391e95e0
shoukailiang <shoukailiang@qq.com>
2019-04-24 20:11:53 +0800

    add 001 007

---
+++ 001_two_sum/TwoSum1.c
@@ -0,0 +1,37 @@
+//两层for循环，思路简单，但时间复杂度为O(n^2)，
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+#include <stdio.h>
+#include <stdlib.h>
+int *twoSum(int *nums, int numsSize, int target)
+{
+  int i;
+  int *arr=NULL;
+  for (i = 0; i < numsSize; i++)
+  {
+    int j;
+    // 不能重复利用数组中的元素
+    for (j = i + 1; j < numsSize; j++)
+    {
+      if (nums[i] + nums[j] == target)
+      { 
+        // 分配大小（题目要求）,malloc 默认是void所以要转换成int *
+        arr= (int *)malloc(sizeof(int)*2);
+        arr[0] = i;
+        arr[1] = j;
+        goto pos;
+      }
+    }
+  }
+pos:
+  return arr;
+}
+int main()
+{
+  int array[] = {1, 2, 3, 4};
+  int *index = twoSum(array, sizeof(array) / sizeof(array[0]), 7);
+  printf("%d %d\n", index[0], index[1]);
+  system("pause");
+  return 0;
+}
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# idea
+# leetcode
+
+| # | Title | Solution | Difficulty |
+| ------ | ------ | ------ | ------ |
+| 001 | [Two Sum](https://leetcode.com/problems/two-sum/description/) | c | Easy |
+| 007 | [Reverse Integer](https://leetcode-cn.com/problems/reverse-integer/) | c | Easy |
