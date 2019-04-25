a89ef6c625a1489ddfd6c7eed3ca2e4389436ad7
shoukailiang <shoukailiang@qq.com>
2019-04-25 16:02:31 +0800

    add 009 013

---
+++ 009_palindrome number/Palindrome Number1.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+bool isPalindrome(int x)
+{
+  long t = x;
+  long j = 0;
+  if (x < 0)
+  {
+    return false;
+  }
+  if (x % 10 == 0 && x / 10 != 0)
+  {
+    return false;
+  }
+  while (x > 0)
+  {
+    j = 10 * j + x % 10;
+    x /= 10;
+  }
+  if (j == t)
+  {
+    return true;
+  }
+  return false;
+}
+
+int main()
+{
+  printf("please input a number \n");
+  int a;
+  scanf("%d", &a);
+  bool result = isPalindrome(a);
+  printf("%d\n", result);
+  system("pause");
+  return 0;
+}
--- README.md
+++ README.md
@@ -4,3 +4,5 @@
 | ------ | ------ | ------ | ------ |
 | 001 | [Two Sum](https://leetcode.com/problems/two-sum/description/) | c | Easy |
 | 007 | [Reverse Integer](https://leetcode-cn.com/problems/reverse-integer/) | c | Easy |
+| 009 | [Palindrome Number](https://leetcode-cn.com/problems/palindrome-number/) | c | Easy |
+| 013 | [Roman to Integer](https://leetcode-cn.com/problems/roman-to-integer/submissions/) | c | Easy |
