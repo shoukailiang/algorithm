bc0b34c17b5d764fbf3c19ca36eef7631498ae9e
shoukailiang <shoukailiang@qq.com>
2022-07-03 12:31:02 +0800

    2022-7-3

--- leetcode/algorithms/21. 合并两个有序链表/1.cpp
+++ leetcode/algorithms/21. 合并两个有序链表/1.cpp
@@ -3,16 +3,18 @@
  * struct ListNode {
  *     int val;
  *     ListNode *next;
- *     ListNode(int x) : val(x), next(NULL) {}
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
 class Solution {
 public:
-    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
-        ListNode* p1 = l1;
-        ListNode* p2 = l2;
-        ListNode* newHead = new ListNode(0);
-        ListNode* r = newHead;
+    ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        ListNode *p1 = list1;
+        ListNode *p2 = list2;
+        ListNode *newhead = new ListNode(0);
+        ListNode *r = newhead;
         while(p1&&p2){
             if(p1->val<p2->val){
                 r->next = p1;
@@ -26,10 +28,10 @@ public:
         }
         if(p1){
             r->next = p1;
-        }else{
+        }
+        if(p2){
             r->next = p2;
         }
-        return newHead->next;
-        
+        return newhead->next;
     }
 };
--- leetcode/algorithms/876. 链表的中间结点/Solution.cpp
+++ leetcode/algorithms/876. 链表的中间结点/Solution.cpp
@@ -1,14 +1,19 @@
- struct ListNode {
-    int val;
-    ListNode *next;
-    ListNode(int x) : val(x), next(NULL) {}
- };
-class Solution1 {
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
 public:
     ListNode* middleNode(ListNode* head) {
         ListNode *p,*q;
-        p = head;
-        q = head;
+        p=head;
+        q=head;
         if(!p){
             return head;
         }
